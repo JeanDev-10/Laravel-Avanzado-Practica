46aea6429a8a9b002cf31508723d452b42877eb1
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-12-30 16:55:55 -0500

    feat: :sparkles: laravel sail ready test redis queues and jobs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -83,12 +83,14 @@ class AuthController extends Controller
                 'email' => 'required|email|unique:users',
                 'password' => 'required|confirmed'
             ]);
-            User::create([
+            $user=User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password) //Encriptamos la contraseña al registrarlo
             ]);
             DB::commit();
+            event(new UserRegisteredEvent($user));
+
             return response()->json([
                 "status" => 1,
                 "msg" => "¡Registro de usuario exitoso!",
